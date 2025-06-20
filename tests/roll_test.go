f2e2d83a3220607d6c92f0c05cc1cba9a3c77502
natanfds <natanfernandesdossantos@zohomail.com>
2025-06-20 00:04:33 -0300

    [feat] Criando teste de rolagens

---
+++ tests/roll_test.go
@@ -0,0 +1,26 @@
+package tests
+
+import (
+	"testing"
+
+	"vtt_api/dice"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestRollDices(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Roll dices Suite")
+}
+
+var _ = Describe("RollService", func() {
+	Context("Rolagem simples", func() {
+		It("Deve rolar um dado de 6 faces", func() {
+			result, err := dice.RollDices("/r 1d6")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(result.Total).To(BeNumerically(">=", 1))
+			Expect(result.Total).To(BeNumerically("<=", 6))
+		})
+	})
+})
